f387f5251c814f0430e9a31a2da8fca8e2bdfe3f
chisun <csjoung@gmail.com>
2023-03-26 17:30:14 +0900

    deploy

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
-import Hello from "./routes/Hello";
+
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/hello" element={<Hello />}></Route>
-        <Route path="/movie:id" element={<Detail />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <Switch>
+        <Route path="/hello">
+          <h1> hello</h1>
+        </Route>
+        <Route path="/movie/:id">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
     </Router>
   );
 }
